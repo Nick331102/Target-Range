f3794863c3a4c1de3161fea107c31ddf12a7f7d6
nick naglieri <nick331102@yahoo.com>
2023-02-03 21:37:38 -0500

    land movement functionality added

--- shooting_range.py
+++ shooting_range.py
@@ -6,19 +6,40 @@ pygame.init()
 screen = pygame.display.set_mode((800, 500))
 # create the clock object.  determines how fast the screen updates
 clock = pygame.time.Clock()
-# load background image
+# load the images
 wood_bg = pygame.image.load("Wood_BG.png")
+land_bg = pygame.image.load("Land_BG.png")
+water_bg = pygame.image.load("Water_BG.png")
+cloud_1 = pygame.image.load("Cloud1.png")
+cloud_2 = pygame.image.load("Cloud2.png")
+
+land_position_y = 370
+land_speed = 1
+
 # Loop until the user clicks the close button.
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    # Copy image to screen:
+    # Copy images to screen:
     screen.blit(wood_bg, (0, 0))
-    # Set the screen background
+    screen.blit(land_bg,(0,land_position_y))
+    screen.blit(water_bg,(0, 420))
+    screen.blit(cloud_1,(100, 5))
+    screen.blit(cloud_2,(500, 10))
+    screen.blit(cloud_1,(300, 20))
+    screen.blit(cloud_2,(200, 15))
+    
+    land_position_y -= land_speed
+    
+    # make land_bg move yp and down.  subtrating from land_position_y makes it move up.  Adding makes it move down
+    if land_position_y == 340 or land_position_y == 370:
+        land_speed *= -1
+    
+     # Set the screen background
     pygame.display.update()
     # game will run at 120 frames per second. Stops the game from running too fast
-    clock.tick(120)
+    clock.tick(60)
 
 
