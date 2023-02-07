10a5f5416f0d6a455b50ef7b183ddcd7c691616d
nick naglieri <nick331102@yahoo.com>
2023-02-07 11:27:49 -0500

    collision mechanics added

--- shooting_range.py
+++ shooting_range.py
@@ -1,5 +1,6 @@
 import pygame 
-import sys          
+import sys     
+import random     
  # Initialize the game engine
 pygame.init()
 # Set the height and width of the screen
@@ -13,7 +14,7 @@ water_bg = pygame.image.load("Water_BG.png")
 cloud_1 = pygame.image.load("Cloud1.png")
 cloud_2 = pygame.image.load("Cloud2.png")
 crosshair = pygame.image.load("crosshair.png")
-duck = pygame.image.load("duck.png")
+duck_surface = pygame.image.load("duck.png")
 
  # set crosshair_rect to the center of the screen   
 crosshair_rect = crosshair.get_rect(center = (400, 250))
@@ -22,7 +23,19 @@ crosshair_rect = crosshair.get_rect(center = (400, 250))
 land_position_y = 370
 land_speed = 1
 water_position_y = 420
-water_speed = .5
+water_speed = .5    
+
+# create a list of 8 duck_rect objects and place them randomly on the screen
+duck_list = []
+for duck in range(8):
+    # randomly placed between 100 and 700 on the x axis and 100 and 250 on the y axis
+    duck_position_x = random.randrange(100, 700)
+    duck_position_y = random.randrange(100, 250)
+    # duck
+    # create a rect object that represents the size and position of the duck
+    duck_rect = duck_surface.get_rect(center = (duck_position_x, duck_position_y)) 
+    duck_list.append(duck_rect)
+
 
 # Loop until the user clicks the close button.
 while True:
@@ -32,15 +45,24 @@ while True:
             sys.exit()
         if event.type == pygame.MOUSEMOTION:
             crosshair_rect = crosshair.get_rect(center = event.pos)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for index,duct_rect in enumerate (duck_list):
+                if crosshair_rect.colliderect(duct_rect):
+                    del duck_list[index]
+                    
+            
     # Copy images to screen:
     screen.blit(wood_bg, (0, 0))
     screen.blit(land_bg,(0,land_position_y))
     screen.blit(water_bg,(0, water_position_y))
+    for duct_rect in duck_list:
+        screen.blit(duck_surface, duct_rect)
     screen.blit(cloud_1,(100, 5))
     screen.blit(cloud_2,(500, 10))
     screen.blit(cloud_1,(300, 20))
     screen.blit(cloud_2,(200, 15))
     screen.blit(crosshair, crosshair_rect)
+    
     
     #When we subtract a negative number from a positive number, it is equivalent to adding a positive version of the negative number.  when land_speed is negative, it is equivalent to adding a positive version of land_speed and when land_speed is positive, it is equivalent to subtracting a positive version of land_speed so when land_posiiton_y is 340, land_speed becomes -1 and then 340 - (-1) is actually adding 1 to land_position_y.  When land_position_y is 370, land_speed becomes 1 and then 370 - 1 is actually subtracting 1 from land_position_y.
     land_position_y -= land_speed
